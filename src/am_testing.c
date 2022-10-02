8936fff60951fae5c549af30d54fceb37fe74457
Shachar Itzhaky <corwin.amber@gmail.com>
2022-10-02 07:59:24 +0300

    [port] Compile stubs to WASM.
    Using wasi-kit. Includes `package.json`.

--- src/am_testing.c
+++ src/am_testing.c
@@ -3,4 +3,4 @@
 /* The default [Base_am_testing] value is [false]. [ppx_inline_test] overrides
    the default by linking against an implementation of [Base_am_testing] that
    returns [true]. */
-CAMLprim CAMLweakdef value Base_am_testing() { return Val_false; }
+CAMLprim CAMLweakdef value Base_am_testing(value tt) { return Val_false; }
